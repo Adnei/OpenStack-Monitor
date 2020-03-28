f1eb3d76dbfb1680e606e1083971f510bbd743fa
Adnei <adneiwillian@gmail.com>
2020-03-28 20:24:52 -0300

    fix import

--- modules/induced_life_cycle.py
+++ modules/induced_life_cycle.py
@@ -14,10 +14,9 @@ from modules.objects import db_info
 from modules.objects.execution import *
 from modules.objects.operation import *
 from modules.objects.metering import *
-from modules.objects.service import *
 from modules.objects.packet_info import *
 from modules.objects.api_info import *
-from modules.objects import service
+from modules.objects.service import *
 from modules.objects import db_info as DB_INFO
 #@TODO: proper indent too long lines
 
