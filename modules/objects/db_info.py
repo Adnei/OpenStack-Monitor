3fc9cc078b6e1ec76f938af60299d6c319bfd96a
Adnei <adneiwillian@gmail.com>
2020-04-08 11:33:36 -0300

    logging handler for sql alchemy

--- modules/objects/db_info.py
+++ modules/objects/db_info.py
@@ -1,12 +1,14 @@
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
-ENGINE = create_engine('sqlite:///network_metering_experiment.db', echo=False)
-BASE = declarative_base(bind=ENGINE)
-SESSIONMAKER = sessionmaker
-
+logging.basicConfig()
 sqlAlchemyHandler = logging.FileHandler('sqlAlchemyLogs.log')
 sqlAlchemyHandler.setLevel(logging.DEBUG)
+
 logging.getLogger('sqlalchemy.engine').addHandler(sqlAlchemyHandler)
+
+ENGINE = create_engine('sqlite:///network_metering_experiment.db', echo="debug")
+BASE = declarative_base(bind=ENGINE)
+SESSIONMAKER = sessionmaker
