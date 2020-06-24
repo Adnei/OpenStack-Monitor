0845675361408034c89505fb11bf8ed2bc1b0815
Adnei <adneiwillian@gmail.com>
2020-06-24 04:53:22 -0300

    verify if temp lsof file exists

--- modules/network_meter.py
+++ modules/network_meter.py
@@ -43,7 +43,7 @@ class NetworkMeter:
     def stopListFiles(self, process, resultFile, tempFilePath='lsof_temp'):
         #Sometimes the operation is too fast and lsof is still in its first iteration.
         #Thus we wait until it finishes at least the first iteration
-        if(os.stat(tempFilePath).st_size == 0):
+        if(os.path.isfile(tempFilePath) == False or os.stat(tempFilePath).st_size == 0):
             defaultLogger.critical('%s (temp file) is empty!! ',tempFilePath)
             defaultLogger.critical('%s could not be created!! Temp file was still empty. Waiting for LSOF.',resultFile)
             time.sleep(1)
